ebbc3bc27d6ab2e3af77a23b6c9f198336390fb1
Yuvraj <random250304@gmail.com>
2025-09-18 10:02:37 +0530

    notes displayed

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, NotesListView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('notes/', NotesListView.as_view(), name='notes'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Note
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class NotesListView(ListView):
+    template_name = 'notes.html'
+    context_object_name = 'notes'
+    model = Note
