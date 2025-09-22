e2936ca80cb2295cbd99d88fb06eeffa63271198
Yuvraj <random250304@gmail.com>
2025-09-22 09:16:06 +0530

    comment model created

--- notes/admin.py
+++ notes/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Comment
 # Register your models here.
-admin.site.register(Note)
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'created_at']
+    inlines = [CommentInLine]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content', 'note', 'user', 'created_at']
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Comment, CommentAdmin)
--- notes/models.py
+++ notes/models.py
@@ -14,3 +14,15 @@ class Note(models.Model):
     
     def get_absolute_url(self):
         return f"/notes/{self.id}"
+    
+
+
+class Comment(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    content = models.TextField()
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
