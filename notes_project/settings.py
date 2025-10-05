5283fb21d0f9efe1cd535ef8b19df3a3bccfd229
Yuvraj <random250304@gmail.com>
2025-10-05 09:11:02 +0530

    allowed hosts updated

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS",subcast=str, default=["localhost"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS",default=[])
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
